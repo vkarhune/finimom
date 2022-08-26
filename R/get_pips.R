075d3d4059a7fa67cb4c1018d9c465e9832acde9
vkarhune <vkarhune@gmail.com>
2022-08-26 13:21:36 +0300

    Add function to extract posterior inclusion probabilities

---
+++ R/get_pips.R
@@ -0,0 +1,17 @@
+#' Title
+#'
+#' @param samples Posterior samples
+#'
+#' @return Posterior inclusion probabilities
+#' @export
+#'
+#' @examples
+get_pips <- function(samples){
+  pip_clusters <- (colSums(samples[[1]] != 0)/nrow(samples[[1]]))
+
+  out <- do.call("rbind", lapply(seq_along(pip_clusters), function(x) {
+    cbind(samples[[5]][[x]], pip_clusters[x]/length(samples[[5]][[x]]))
+  }))
+
+  out <- out[order(out[,1]),]
+}
