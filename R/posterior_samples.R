186aa5aa37834c948912b777b38fba11f5d664fc
vkarhune <vkarhune@gmail.com>
2022-09-27 15:30:55 +0300

    Fix posterior_samples() function

--- R/posterior_samples.R
+++ R/posterior_samples.R
@@ -12,28 +12,23 @@
 #' @param p Number of variants.
 #' @param seed Random seed.
 #' @param excl.burnin Should the burn-in be excluded?
-#' @param n Sample size (not used).
 #' @param a0 Hyperparameter a for the model size prior.
 #' @param b0 Hyperparameter b for the model size prior.
 #' @param inds0 Initial model indices (not used).
 #' @param standardize Should the effect sizes be standardised? Defaults to TRUE.
-#' @param msprior Model size prior (not used).
 #' @param verbose Verbose output.
 #' @param clump
 #' @param clump_r2
 #' @param check_ld
-#' @param anchor
-#' @param anchor_cslevel
 #'
 #' @return List.
 #' @export
 #'
 #' @examples
-posterior_samples <- function(
+posterior_samples2 <- function(
     beta, se, eaf, R, maxsize, tau0, r0, niter, burnin, p, seed = 456, excl.burnin = TRUE,
-    n, a0 = 0.05, b0 = 0.95, inds0 = NULL, standardize = TRUE,
-    msprior = NULL, verbose = TRUE,
-    clump = TRUE, clump_r2 = 0.99^2, check_ld = FALSE, anchor = FALSE, anchor_cslevel = 0.5){
+    a0 = 1, b0 = NULL, inds0 = NULL, standardize = TRUE,
+    verbose = TRUE, clump = TRUE, clump_r2 = 0.99^2, check_ld = FALSE){
 
 
   if(0){
@@ -55,6 +50,7 @@ posterior_samples <- function(
     clump_r2 <- 0.99^2
   }
 
+  if(is.null(b0)) { b0 <- p^1.5 }
 
   if(standardize){
     beta <- beta*sqrt(2*eaf*(1-eaf))
@@ -70,35 +66,9 @@ posterior_samples <- function(
 
     if(check_ld){
       ldcheck <- lapply(keeplist_cleaned, check_ld_disc, z = z,
-      # ldcheck <- lapply(keeplist_cleaned[1:5], check_ld_disc, z = z,
+                        # ldcheck <- lapply(keeplist_cleaned[1:5], check_ld_disc, z = z,
                         Chi2_quantile = 0.5,
                         LDm = R, clump_r2 = clump_r2)
-
-      #KESKEN simply edit the groups, but do not touch the LD matrix
-      if(0){
-
-      ld_changes <- lapply(ldcheck, function(x){
-        if(!(is.list(x))){
-          out <- NULL
-        } else {
-          out <- x[[2]]
-        }
-        return(out)
-      })
-
-      ld_changes <- ld_changes[sapply(ld_changes, function(x) !(is.null(x)))]
-
-      invisible(lapply(ld_changes, function(x){
-        R[x[1],x[2]] <<- x[3]
-        R[x[2],x[1]] <<- x[3]
-      }))
-
-
-      ldcheck <- lapply(ldcheck, "[[", 1)
-
-
-      keeplist_cleaned <- lapply(rapply(ldcheck, enquote, how = "unlist"), eval)
-      }
 
       keeplist_cleaned <- lapply(rapply(ldcheck, enquote, how = "unlist"), eval)
 
@@ -127,15 +97,11 @@ posterior_samples <- function(
   }
 
 
-  if(is.null(msprior)) { msprior <- "complexity" }
+  msprior <- "complexity"
 
   if(msprior %in% "complexity") {
     lprior <- sapply(seq_len(maxsize), dbb, p = p, a = a0, b = b0)
     lprior <- log(exp(lprior)/sum(exp(lprior)))
-  }
-
-  if(msprior %in% "uniform") {
-    lprior <- log(rep(1/maxsize, maxsize))
   }
 
   # initialise
@@ -145,30 +111,21 @@ posterior_samples <- function(
     inds <- inds0
   }
 
-  if(anchor){
-  # anchor 95% credible set
-  lbf <- bf(beta, se, tau, r0)
-
-  pp <- exp(lbf)/sum(exp(lbf))
-
-  anchorset <- order(pp, decreasing = T)[which(cumsum(sort(pp, decreasing = TRUE)/sum(pp)) < anchor_cslevel)]
-  }
-
 
 
   opt <- stats::optim(as.matrix(beta[inds], ncol = 1), g,
-               beta = as.matrix(beta[inds], ncol = 1),
-               se = se[inds],
-               R = R[inds,inds,drop = F],
-               psi = 1,
-               k = length(inds), tau = tau0, r = r0,
-               # method = "BFGS")
-               method = "Nelder-Mead", control = list(warn.1d.NelderMead = FALSE))
+                      beta = as.matrix(beta[inds], ncol = 1),
+                      se = se[inds],
+                      R = R[inds,inds,drop = F],
+                      psi = 1,
+                      k = length(inds), tau = tau0, r = r0,
+                      # method = "BFGS")
+                      method = "Nelder-Mead", control = list(warn.1d.NelderMead = FALSE))
 
   lml <- logmyb(as.matrix(beta[inds], ncol = 1),
-               se[inds],
-               tau = tau0, psi = 1, r = r0, k = length(inds),
-               R = R[inds,inds,drop = F], gval = opt$value)
+                se[inds],
+                tau = tau0, psi = 1, r = r0, k = length(inds),
+                R = R[inds,inds,drop = F], gval = opt$value)
   lp <- lml + logmk(lprior, length(inds))
 
   modelsize <- length(inds)
@@ -194,21 +151,19 @@ posterior_samples <- function(
 
     # i <- i + 1
 
-    fullrank <- FALSE
-    while(!fullrank){
 
-    # randomly pick add = 1, delete = 0, swap = 2
-    if(modelsize == 1){
-      add <- sample(1:2, size = 1)
-    } else if(modelsize == maxsize){
-      if(modelsize == p){
-        add <- 0
+      # randomly pick add = 1, delete = 0, swap = 2
+      if(modelsize == 1){
+        add <- sample(1:2, size = 1)
+      } else if(modelsize == maxsize){
+        if(modelsize == p){
+          add <- 0
+        } else {
+          add <- sample(c(0, 2), size = 1)
+        }
       } else {
-        add <- sample(c(0, 2), size = 1)
+        add <- sample(0:2, size = 1)
       }
-    } else {
-      add <- sample(0:2, size = 1)
-    }
 
       if(add == 1){
         # swapindex <- sample(which(betavec == 0), 1)
@@ -229,33 +184,31 @@ posterior_samples <- function(
       }
 
       cond <- qr(R[indsprop,indsprop])$rank == length(indsprop)
-      if(anchor) { cond <- cond & any(indsprop %in% anchorset) }
 
-      if(cond){
-        fullrank <- TRUE
-      }
-
-    }
 
     # inds <- sort(c(inds, newinds))
 
+      if(!(cond)){
+        lpnew <- Inf
+      } else {
+
     opt <- stats::optim(as.matrix(beta[indsprop], ncol = 1),
-                 g,
-                 beta = as.matrix(beta[indsprop], ncol = 1),
-                 se = se[indsprop],
-                 R = R[c(indsprop),c(indsprop),drop = F],
-                 psi = 1,
-                 k = length(indsprop), tau = tau0, r = r0,
-                 # method = "BFGS")
-                 method = "Nelder-Mead", control = list(warn.1d.NelderMead = FALSE))
+                        g,
+                        beta = as.matrix(beta[indsprop], ncol = 1),
+                        se = se[indsprop],
+                        R = R[c(indsprop),c(indsprop),drop = F],
+                        psi = 1,
+                        k = length(indsprop), tau = tau0, r = r0,
+                        # method = "BFGS")
+                        method = "Nelder-Mead", control = list(warn.1d.NelderMead = FALSE))
 
 
 
     lmlnew <- logmyb(as.matrix(beta[indsprop], ncol = 1),
-                    se[indsprop],
-                    tau = tau0, psi = 1, r = r0, k = length(indsprop),
-                    R = R[c(indsprop),c(indsprop),drop = F],
-                    gval = opt$value)
+                     se[indsprop],
+                     tau = tau0, psi = 1, r = r0, k = length(indsprop),
+                     R = R[c(indsprop),c(indsprop),drop = F],
+                     gval = opt$value)
     lpnew <- lmlnew + logmk(lprior, length(indsprop))
 
     betaprop <- rep(0, p)
@@ -269,6 +222,8 @@ posterior_samples <- function(
     # llr <- lpnew - lp
     # barker <- exp(lpnew)/(exp(lp) + exp(lpnew))
     barker <- 1/(1+exp(lp - lpnew))
+
+      }
 
     if(is.infinite(lpnew)) { barker <- -1 }
     # if(opt$convergence %in% c(1, 10)) { barker <- -1 }
@@ -296,15 +251,17 @@ posterior_samples <- function(
   cat(sprintf("%i iterations done in %.2f seconds\n", niter, (proc.time() - prc)[[3]]))
 
   if(excl.burnin){
-  out <- list(out[[1]][(burnin + 1):niter,],
-              out[[2]][(burnin + 1):niter],
-              out[[3]][(burnin + 1):niter],
-              out[[4]][(burnin + 1):niter])
+    out <- list(out[[1]][(burnin + 1):niter,],
+                out[[2]][(burnin + 1):niter],
+                out[[3]][(burnin + 1):niter],
+                out[[4]][(burnin + 1):niter])
   }
 
   if(clump){
     out <- c(out,
              list(keeplist_cleaned))
+  } else {
+    out <- c(out, list(seq_len(p)))
   }
 
   return(out)
