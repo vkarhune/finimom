017e5c2608739c78cb0d74f5702e4ff7db6c201c
vkarhune <vkarhune@gmail.com>
2022-10-11 15:39:13 +0300

    Add imom density function

---
+++ R/dimom.R
@@ -0,0 +1,19 @@
+#' Title
+#'
+#' @param x
+#' @param r
+#' @param tau
+#' @param phi
+#'
+#' @return
+#' @export
+#'
+#' @examples
+#'
+dimom <- Vectorize(function(x, r, tau, phi){
+  if(x == 0){
+    0
+  } else {
+    ((tau*phi)^(r/2)/gamma(r/2))*abs(x)^(-(r+1))*exp(-tau*phi/x^2)
+  }
+})
