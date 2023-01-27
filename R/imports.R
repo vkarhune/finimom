559c5b1f0b6020093bd83b0e72563fd487adfed8
vkarhune <vkarhune@gmail.com>
2023-01-27 19:15:01 +0200

    Fix Rcpp set-up

--- R/imports.R
+++ R/imports.R
@@ -1,3 +1,5 @@
 #' @importFrom stats optim optimise qchisq qnorm runif
 #' @importFrom utils setTxtProgressBar txtProgressBar
+#' @importFrom Rcpp evalCpp
+#' @useDynLib finimom, .registration = TRUE
 NULL
