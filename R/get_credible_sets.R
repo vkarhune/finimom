be559c3866fb360e73a4f4661d10eaadf2396099
vkarhune <vkarhune@gmail.com>
2023-10-26 18:15:07 +0300

    Fix get_credible_sets() function

--- R/get_credible_sets.R
+++ R/get_credible_sets.R
@@ -12,6 +12,12 @@
 #' @examples
 get_credible_sets <- function(samples, num_signals, level = 0.95, purity = purity, R = R){
 
+  if(all(is.na(samples))) { stop("Make sure you are using the correct object, or that maxsize > 1") }
+
+  if(!(num_signals %in% unique(samples[[2]]))) {
+    stop(sprintf("The posterior do not support the requested number of signals (%i) in the data.\n", num_signals))
+  }
+
   ctable <- sort(table(samples[[3]][samples[[2]] %in% num_signals]), decreasing = T)
   csind <- ctable |> prop.table() |> cumsum() |> (\(.) min(which(. > level)))()
 
