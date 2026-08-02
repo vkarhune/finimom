6c63fd36e720e14b715985a8554e2ad592e175a1
vkarhune <vkarhune@gmail.com>
2026-08-02 15:38:12 +0100

    Fix function to calculate C matrix

--- R/create_Cmatrix.R
+++ R/create_Cmatrix.R
@@ -1,6 +1,11 @@
 create_Cmatrix <- function(rho){
- edrho <- eigen(matrix(c(1, rho, rho, 1), nrow = 2))
- S <- edrho$vectors %*% diag(edrho$values^(-0.5)) %*% t(edrho$vectors)
- out <- stats::cov2cor(S)
- return(out)
+
+  if(length(rho) == 1){
+    edrho <- eigen(matrix(c(1, rho, rho, 1), nrow = 2))
+  } else {
+    edrho <- eigen(rho)
+  }
+  S <- edrho$vectors %*% diag(edrho$values^(-0.5)) %*% t(edrho$vectors)
+  out <- cov2cor(S)
+  return(out)
 }
