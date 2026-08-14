a01fa4b00e3cd4bec0ef979eae84f850789ffd5b
vkarhune <vkarhune@gmail.com>
2026-08-14 15:22:28 +0100

    Do not return credible sets if the same variant is in multiple sets

--- R/get_multitrait_credible_sets.R
+++ R/get_multitrait_credible_sets.R
@@ -106,4 +106,12 @@ credible_sets
 
 })
 
+
+dups <- sapply(sets1, function(x) any(duplicated(unlist(x))))
+if(any(dups)){
+  warning("Same variant in multiple credible sets for one trait - no credible sets provided. This is usually a symptom of an LD mismatch")
+  sets1 <- lapply(seq_along(sets1), function(x) NULL )
+}
+
+
 }
